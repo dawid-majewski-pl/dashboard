9187e329ceeef04e251285f960b4119fcba20d97
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-07-26 10:33:07 +0200

    Menu, Navbar, Footer, Login components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,62 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet
 } from "react-router-dom";
 
 import Home from "./pages/home/Home"
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+
+import "./styles/global.scss"
+
+const Layout = () => {
+  return(
+    <div>
+      <Navbar />
+      <div className="Container">
+        <div className="MenuContainer">
+          <Menu />
+        </div>
+        <div className="ContentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-       <Home />
-      ),
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+      ]
     },
     {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
-    },
+      path: "/Login",
+      element: <Login />,
+    }
+
   ]);
   return (
     <div>
