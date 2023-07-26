12cd236f19992500d54fd5f59d568b7c35ff088f
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-07-26 11:35:48 +0200

    Global styles and scss variables added.

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,31 @@ import {
 import Home from "./pages/home/Home"
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Login from "./pages/login/Login";
 import Navbar from "./components/navbar/Navbar";
 import Menu from "./components/menu/Menu";
 import Footer from "./components/footer/Footer";
-import Login from "./pages/login/Login";
 
 import "./styles/global.scss"
 
-const Layout = () => {
-  return(
-    <div>
-      <Navbar />
-      <div className="Container">
-        <div className="MenuContainer">
-          <Menu />
-        </div>
-        <div className="ContentContainer">
-          <Outlet />
-        </div>
-      </div>
-      <Footer />
-    </div>
-  )
-}
-
 function App() {
+
+  const Layout = () => {
+    return(
+      <div className="Main">
+        <Navbar />
+        <div className="Container">
+          <div className="MenuContainer">
+            <Menu />
+          </div>
+          <div className="ContentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
 
   const router = createBrowserRouter([
     {
