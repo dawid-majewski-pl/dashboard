12894db74405568d41fa97b65d2851c87c6cfce2
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-07-27 10:02:00 +0200

    Home component grid

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,17 @@ import "./home.scss"
 
 const Home = () => {
     return (
-        <div className="Home">Home</div>
+        <div className="Home">
+        <div className="Home__box Home__box--1">1</div>
+        <div className="Home__box Home__box--2">2</div>
+        <div className="Home__box Home__box--3">3</div>
+        <div className="Home__box Home__box--4">4</div>
+        <div className="Home__box Home__box--5">5</div>
+        <div className="Home__box Home__box--6">6</div>
+        <div className="Home__box Home__box--7">7</div>
+        <div className="Home__box Home__box--8">8</div>
+        <div className="Home__box Home__box--8">9</div>
+        </div>
     )
 }
 
