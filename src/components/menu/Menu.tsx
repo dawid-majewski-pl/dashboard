ffd0e35d5a902ee93284eff87ea0a0bd344912ec
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-07-27 09:43:57 +0200

    Menu data import form data.ts

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,94 +1,25 @@
+import { Link } from "react-router-dom";
+
+import { menu } from "../../data";
 import "./menu.scss";
-import home from "/src/svg/home.svg";
-import profile from "/src/svg/profile.svg";
-import products from "/src/svg/product.svg";
-import orders from "/src/svg/order.svg";
-import posts from "/src/svg/post2.svg";
-import elements from "/src/svg/element.svg";
-import notes from "/src/svg/note.svg";
-import forms from "/src/svg/form.svg";
-import calendar from "/src/svg/calendar.svg";
-import settings from "/src/svg/settings.svg";
-import backups from "/src/svg/backup.svg";
-import charts from "/src/svg/chart.svg";
-import logs from "/src/svg/log.svg";
+
 
 const Menu = () => {
     return (
         <div className="Menu">
-            <div className="Menu__item">
-                <span className="Menu__title">MAIN</span>
-                <a className="Menu__link" href="x">
-                    <img src={home} alt="Home icon." />
-                    <span className="Menu__link-title">Home</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={profile} alt="Profile icon." />
-                    <span className="Menu__link-title">Profile</span>
-                </a>
+            {menu.map((item) => (
+            <div className="Menu__item" key={item.id}>
+                <span className="Menu__title" key={item.title}></span>
+                {item.listItems.map((listItem) => (
+                <Link className="Menu__link" to={listItem.url}>
+                    <img src={listItem.icon} alt={listItem.alt} />
+                    <span className="Menu__link-title">{listItem.title}</span>
+                </Link>
+                ))}
             </div>
-            <div className="Menu__item">
-                <span className="Menu__title">LISTS</span>
-                <a className="Menu__link" href="x">
-                    <img src={home} alt="Home icon." />
-                    <span className="Menu__link-title">Users</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={products} alt="Profile icon." />
-                    <span className="Menu__link-title">Products</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={orders} alt="Home icon." />
-                    <span className="Menu__link-title">Orders</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={posts} alt="Profile icon." />
-                    <span className="Menu__link-title">Posts</span>
-                </a>
-            </div>
-            <div className="Menu__item">
-                <span className="Menu__title">GENERAL</span>
-                <a className="Menu__link" href="x">
-                    <img src={elements} alt="Home icon." />
-                    <span className="Menu__link-title">Elements</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={notes} alt="Profile icon." />
-                    <span className="Menu__link-title">Notes</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={forms} alt="Home icon." />
-                    <span className="Menu__link-title">Forms</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={calendar} alt="Profile icon." />
-                    <span className="Menu__link-title">Calendar</span>
-                </a>
-            </div>
-            <div className="Menu__item">
-                <span className="Menu__title">MAINTENANCE</span>
-                <a className="Menu__link" href="x">
-                    <img src={settings} alt="Home icon." />
-                    <span className="Menu__link-title">Settings</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={backups} alt="Profile icon." />
-                    <span className="Menu__link-title">Backups</span>
-                </a>
-            </div>
-            <div className="Menu__item">
-                <span className="Menu__title">ANALYTICS</span>
-                <a className="Menu__link" href="x">
-                    <img src={charts} alt="Home icon." />
-                    <span className="Menu__link-title">Charts</span>
-                </a>
-                <a className="Menu__link" href="x">
-                    <img src={logs} alt="Profile icon." />
-                    <span className="Menu__link-title">Logs</span>
-                </a>
-            </div>
+        ))}
         </div>
-    );
+    )
 };
 
 export default Menu;
---
+++ src/data.ts
@@ -0,0 +1,130 @@
+export const menu = [
+    {
+        id: 1,
+        title: "main",
+        listItems: [
+            {
+                id: 1,
+                title: "Homepage",
+                url: "/",
+                icon: "/src/svg/home.svg",
+                alt: "Home icon."
+            },
+            {
+                id: 2,
+                title: "Profile",
+                url: "users/1",
+                icon: "/src/svg/profile.svg",
+                alt: "Profile icon."
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "lists",
+        listItems: [
+            {
+                id: 1,
+                title: "Users",
+                url: "/users",
+                icon: "/src/svg/user.svg",
+                alt: "User icon."
+            },
+            {
+                id: 2,
+                title: "Produts",
+                url: "/products",
+                icon: "/src/svg/product.svg",
+                alt: "Product icon."
+            },
+            {
+                id: 3,
+                title: "Orders",
+                url: "/orders",
+                icon: "/src/svg/order.svg",
+                alt: "Order icon."
+            },
+            {
+                id: 4,
+                title: "Posts",
+                url: "/posts",
+                icon: "/src/svg/post2.svg",
+                alt: "Post icon."
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "general",
+        listItems: [
+            {
+                id: 1,
+                title: "Elements",
+                url: "/elements",
+                icon: "/src/svg/element.svg",
+                alt: "Elements icon."
+            },
+            {
+                id: 2,
+                title: "Notes",
+                url: "/notes",
+                icon: "/src/svg/note.svg",
+                alt: "Notes icon."
+            },
+            {
+                id: 3,
+                title: "Forms",
+                url: "/forms",
+                icon: "/src/svg/form.svg",
+                alt: "Form icon."
+            },
+            {
+                id: 4,
+                title: "Calendar",
+                url: "/calendar",
+                icon: "/src/svg/calendar.svg",
+                alt: "Calendar icon."
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "maintenance",
+        listItems: [
+            {
+                id: 1,
+                title: "Settings",
+                url: "/settings",
+                icon: "/src/svg/settings.svg",
+                alt: "Settings icon."
+            },
+            {
+                id: 2,
+                title: "Backup",
+                url: "/backup",
+                icon: "/src/svg/backup.svg",
+                alt: "Backup icon."
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "analytics",
+        listItems: [
+            {
+                id: 1,
+                title: "Charts",
+                url: "/charts",
+                icon: "/src/svg/chart.svg",
+                alt: "Charts icon."
+            },
+            {
+                id: 2,
+                title: "Logs",
+                url: "/logs",
+                icon: "/src/svg/log.svg",
+                alt: "Logs icon."
+            },
+        ],
+    },
+]
